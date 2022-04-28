9572f9b2552117c9945eee2455fc967fdee48198
suraj <smarty27c@gmail.com>
2022-04-28 10:14:17 +0530

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>{{ counter }}</h2>
+    <input name="add" v-model="counterByValue" />
+    <button name="addCount" @click="updateCounterByValue">
+      Increment counter By Value
+    </button>
+    <button name="addCount" @click="updateCounter">Increment counter</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Counter",
+
+  data() {
+    return {
+      counter: 0,
+      counterByValue: 0,
+    };
+  },
+  props: {
+    namev: String,
+  },
+  methods: {
+    updateCounter() {
+      this.counter += 1;
+    },
+    updateCounterByValue() {
+      this.counter = this.counter + parseInt(this.counterByValue);
+    },
+  },
+  filters: {
+    caps: function (val) {
+      val = val.toString();
+      return val.charAt(0).toUpperCase() + val.slice(1);
+    },
+  },
+};
+</script>
